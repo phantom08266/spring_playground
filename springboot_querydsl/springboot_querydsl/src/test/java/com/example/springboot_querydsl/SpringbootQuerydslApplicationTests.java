bd9a0570924d144975fd08831091ba823c24564d
phantom08266 <phantom08266@gmail.com>
2023-10-27 16:40:16 +0900

    [QueryDsl] 프로젝트 생성 및 QueryDsl 연동
    - springboot 2.7.17
    - java 17

---
+++ springboot_querydsl/springboot_querydsl/src/test/java/com/example/springboot_querydsl/SpringbootQuerydslApplicationTests.java
@@ -0,0 +1,37 @@
+package com.example.springboot_querydsl;
+
+import com.example.springboot_querydsl.entity.Hello;
+import com.example.springboot_querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class SpringbootQuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+        Hello result = query.selectFrom(qHello).fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+
+}
