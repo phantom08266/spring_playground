d9f8462011d778a97cf033a72d821c0c032d3242
phantom08266 <phantom08266@gmail.com>
2023-10-31 13:07:19 +0900

    [QueryDsl] Entity 설계 및 단위테스트 작성

---
+++ springboot_querydsl/springboot_querydsl/src/main/java/com/example/springboot_querydsl/entity/Member.java
@@ -0,0 +1,43 @@
+package com.example.springboot_querydsl.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+    public Member(String username, int age, Team newTeam) {
+        this.username = username;
+        this.age = age;
+        if (newTeam != null) {
+            changeTeam(newTeam);
+        }
+    }
+
+    public void changeTeam(Team newTeam) {
+        this.team = newTeam;
+        newTeam.getMembers().add(this);
+    }
+}
---
+++ springboot_querydsl/springboot_querydsl/src/main/java/com/example/springboot_querydsl/entity/Team.java
@@ -0,0 +1,25 @@
+package com.example.springboot_querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
