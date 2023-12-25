eb3e81b791adbb992677787c1b21ee8f6a83f015
phantom08266 <phantom08266@gmail.com>
2023-12-25 22:57:17 +0900

    [Feign Client] Feign 커스텀 Logger 글로벌 빈으로 등록
    - 보통 로그는 글로벌로 설정함.

--- feignClient/src/main/java/com/example/feignclient/config/FeignConfig.java
+++ feignClient/src/main/java/com/example/feignclient/config/FeignConfig.java
@@ -1,7 +1,15 @@
 package com.example.feignclient.config;
 
+import com.example.feignclient.feign.logger.FeignCustomLogger;
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+
+    @Bean
+    public Logger logger() {
+        return new FeignCustomLogger();
+    }
 }
