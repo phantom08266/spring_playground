ccd835f3baa0ea76933028e7fced281e9422ee0d
phantom08266 <phantom08266@gmail.com>
2023-12-25 21:49:41 +0900

    [Feign Client] Feign interceptor 빈 추가

--- feignClient/src/main/java/com/example/feignclient/feign/config/DemoFeignConfig.java
+++ feignClient/src/main/java/com/example/feignclient/feign/config/DemoFeignConfig.java
@@ -1,5 +1,13 @@
 package com.example.feignclient.feign.config;
 
 
+import com.example.feignclient.feign.interceptor.DemoFeignInterceptor;
+import org.springframework.context.annotation.Bean;
+
 public class DemoFeignConfig {
+
+    @Bean
+    public DemoFeignInterceptor demoFeignInterceptor() {
+        return DemoFeignInterceptor.ofDefault();
+    }
 }
