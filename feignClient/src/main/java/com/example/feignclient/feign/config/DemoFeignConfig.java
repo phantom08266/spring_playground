dba39588d5e5631052a84bdea352bad293bb4e3d
phantom08266 <phantom08266@gmail.com>
2023-12-25 22:57:57 +0900

    [Feign Client] 누락된 Config 애노테이션 추가

--- feignClient/src/main/java/com/example/feignclient/feign/config/DemoFeignConfig.java
+++ feignClient/src/main/java/com/example/feignclient/feign/config/DemoFeignConfig.java
@@ -3,7 +3,9 @@ package com.example.feignclient.feign.config;
 
 import com.example.feignclient.feign.interceptor.DemoFeignInterceptor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DemoFeignConfig {
 
     @Bean
