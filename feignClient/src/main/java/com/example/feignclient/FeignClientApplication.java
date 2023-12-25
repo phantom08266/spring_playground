a1593722a2a6857750a8f3608a05d677a07024a8
phantom08266 <phantom08266@gmail.com>
2023-12-25 16:49:03 +0900

    [Feign Client] Feign Client 사용하기 위한 애노테이션 추가

--- feignClient/src/main/java/com/example/feignclient/FeignClientApplication.java
+++ feignClient/src/main/java/com/example/feignclient/FeignClientApplication.java
@@ -2,7 +2,9 @@ package com.example.feignclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients // FeignClient를 사용하기 위해 필요한 어노테이션
 @SpringBootApplication
 public class FeignClientApplication {
 
