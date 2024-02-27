ad2c7cffc4ed3d4d1089f1a47531d218b6bfe20b
phantom08266 <phantom08266@gmail.com>
2024-02-27 23:25:04 +0900

    [Reactive_tech] 프로젝트 생성

---
+++ reactive_tech/src/main/java/com/example/reactive_tech/ReactiveTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactive_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveTechApplication.class, args);
+    }
+
+}
---
+++ reactive_tech/src/test/java/com/example/reactive_tech/ReactiveTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactive_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
