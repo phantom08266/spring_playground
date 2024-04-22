c17402df054f3c37def5942d78d41d9efe6f6654
phantom08266 <phantom08266@gmail.com>
2024-04-22 22:50:16 +0900

    [vueJS] 프로젝트 최초 커밋

---
+++ vueJS-springboot/src/main/java/com/example/vuejsspringboot/VueJsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuejsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueJsSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueJsSpringbootApplication.class, args);
+    }
+
+}
---
+++ vueJS-springboot/src/test/java/com/example/vuejsspringboot/VueJsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuejsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueJsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
