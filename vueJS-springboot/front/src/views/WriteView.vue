0aadfed4650b4d597951992ecd4d996f5a588b21
phantom08266 <phantom08266@gmail.com>
2024-05-01 18:19:19 +0900

    [vueJS] elementPlus css하고 axios를 통한 요청처리

--- vueJS-springboot/front/src/views/WriteView.vue
+++ vueJS-springboot/front/src/views/WriteView.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
 
+import {ref} from "vue";
+import axios from "axios";
+
+const title = ref('');
+const content = ref('');
+
+const write = function () {
+  axios.post("http://localhost:8080/api/posts",{
+    title: title.value,
+    content: content.value
+  })
+  console.log(title.value, content.value)
+  // alert(title + " / " + content)
+}
 </script>
 
 <template>
   <div>
-    <input type="text" placeholder="제목을 입력해주세요"/>
+    <el-input v-model="title" placeholder="제목을 입력해주세요"/>
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="20"/>
   </div>
 
   <div>
-    <textarea rows="20"/>
+    <el-button type="primary" @click="write()">글 작성완료</el-button>
   </div>
 
-  <button>글 작성완료</button>
 </template>
 <style>
 </style>
