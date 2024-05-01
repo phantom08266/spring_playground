87fc4519ce85591c652fb8351dd307a51c8b29d8
phantom08266 <phantom08266@gmail.com>
2024-05-01 17:17:48 +0900

    [vueJS] vue.js 관련 font코드 최초 추가

---
+++ vueJS-springboot/front/src/views/WriteView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <input type="text" placeholder="제목을 입력해주세요"/>
+  </div>
+
+  <div>
+    <textarea rows="20"/>
+  </div>
+
+  <button>글 작성완료</button>
+</template>
+<style>
+</style>
